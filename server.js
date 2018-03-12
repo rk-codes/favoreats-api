418a31541dccc1ab55d8e75c1d524adfbbbf258c
rk-codes <ping.you.back@gmail.com>
2018-03-12 12:01:32 -0700

    add restaurant router

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const app = express();
 const cors = require('cors');
 const {CLIENT_ORIGIN, PORT} = require('./config');
 
+//Routers
+const {router: restaurantRouter} = require('./restaurant');
+
 app.use(
     cors({
         origin: CLIENT_ORIGIN
     })
 );
+
+app.use('/restaurants', restaurantRouter);
 
 app.get('/api/*', (req, res) => {
   res.json({ok: true});

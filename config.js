e959e0ade7325abab817fdc07394870ddfacd7c9
rk-codes <ping.you.back@gmail.com>
2018-03-12 11:38:12 -0700

    add local mongo db

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
 exports.PORT = process.env.PORT || 8080;
+exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/favoreats-app';

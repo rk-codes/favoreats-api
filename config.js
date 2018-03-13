d3602da28882c1493c37466fa92cc2b76ccd5168
rk-codes <ping.you.back@gmail.com>
2018-03-12 18:35:32 -0700

    add jwt key

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
 exports.PORT = process.env.PORT || 8080;
 exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/favoreats-app';
+exports.JWT_SECRET = process.env.JWT_SECRET || 'secret';
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';

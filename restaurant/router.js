b65ae043ff8ad56ff90837f914428afbe49ba5a3
rk-codes <ping.you.back@gmail.com>
2018-03-13 00:45:19 -0700

    modify findByIdAndUpdate to return updated data

--- restaurant/router.js
+++ restaurant/router.js
@@ -60,11 +60,10 @@ router.post('/', jsonParser, jwtAuth, (req, res) => {
     })
     .then(restaurant => 
         User.findByIdAndUpdate(req.user.id, {
-            $push: {"restaurants": restaurant}
-        })      
+            $push: {"restaurants": restaurant},        
+        }, {'new': true})      
     )
-    .then(user => User.findById(req.user.id))
-    .then(userdata => res.status(201).json(userdata.serialize()))
+    .then(user => res.status(201).json(user.serialize()))
     .catch(err => {
         console.error(err);
         res.status(500).json({ error: 'Internal Server Error' });
@@ -75,10 +74,9 @@ router.post('/', jsonParser, jwtAuth, (req, res) => {
 router.delete('/:id', jwtAuth, (req, res) => {
     Restaurant.findByIdAndRemove(req.params.id)
     .then(restaurant => 
-        User.findByIdAndUpdate({_id: restaurant.user}, {$pull: {restaurants: req.params.id}})
+        User.findByIdAndUpdate({_id: restaurant.user}, {$pull: {restaurants: req.params.id}}, {'new': true})
     )
-    .then(user => User.findById(req.user.id))
-    .then(userdata => res.json(userdata.serialize()))
+    .then(user => res.json(user.serialize()))
     .catch(err => {
         console.error(err);
         res.status(500).json({ error: 'Internal Server Error' });
@@ -87,7 +85,29 @@ router.delete('/:id', jwtAuth, (req, res) => {
 
 //Update a restaurant
 router.put('/:id', jsonParser, jwtAuth, (req, res) => {
-   
+   // ensure that the id in the request path and the one in request body match
+   if(!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = (
+        `Request path id (${req.params.id}) and request body id ` +
+        `(${req.body.id}) must match`);
+        console.error(message);
+        return res.status(400).json({ message: message });
+    }
+    const toUpdate = {};
+    const updateableFields = ['name', 'location', 'cuisine'];
+    updateableFields.forEach(field => {
+        if (field in req.body) {
+        toUpdate[field] = req.body[field];
+        }
+    });
+    console.log(toUpdate);
+    Restaurant.findByIdAndUpdate(req.params.id, {'new': true}, {$set: toUpdate})
+   // .then(restaurant => Restaurant.findById(req.params.id))
+    .then(restaurant => res.status(200).json(restaurant))
+    .catch(err => {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    })
 })
 
 module.exports = {router};

8630527e7ba01bf0b29b1ede9ac32f4385ecfc68
rk-codes <ping.you.back@gmail.com>
2018-03-15 18:33:20 -0700

    add endpoints to post and get dish

--- restaurant/router.js
+++ restaurant/router.js
@@ -8,7 +8,9 @@ const { router: authRouter, localStrategy, jwtStrategy } = require('../auth');
 const jwtAuth = passport.authenticate('jwt', { session: false });
 
 const {Restaurant} = require('./models');
-const {User} = require('../user//models');
+const {User} = require('../user/models');
+const {Dish} = require('../dish/models');
+const {Review} = require('../review/models');
 
 const jsonParser = bodyParser.json();
 const router = express.Router();
@@ -114,6 +116,73 @@ router.put('/:id', jsonParser, jwtAuth, (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     })
 })
+
+//GET all dishes of a restaurant
+router.get('/:id/dishes', jwtAuth, (req, res) => {
+    Restaurant.findById(req.params.id).populate({path: 'dishes'})
+    .then(restaurant => res.status(200).json(restaurant.dishes))
+    .catch(err => {
+        console.error(err);
+        res.status(500).json({error: 'Internal Server Error'});
+  });
+})
+
+// GET a dish by id
+router.get('/:id/dishes/:dishId', jwtAuth, (req, res) => {
+    Dish.findById(req.params.dishId).populate({path: 'reviews'})
+    .then(dish => {
+        if(dish.restaurant.toString() === req.params.id) {
+            res.status(200).json(dish)
+        }
+        else{
+            res.status(400).json("Restaurant does'nt contain the dish");
+          }  
+    })
+    .catch(err => {
+        console.error(err);
+        res.status(500).json({error: 'Internal Server Error'});
+  });
+})
+
+//POST a dish to a restaurant
+router.post('/:id/dishes', jwtAuth, jsonParser, (req, res) => {
+    const requiredFields = ['name', 'rating', 'description'];
+    for (let i = 0; i < requiredFields.length; i++) {
+        const field = requiredFields[i];
+        if (!(field in req.body)) {
+            const message = `Missing \`${field}\` in request body`;
+            console.error(message);
+            return res.status(400).send(message);
+        }
+      }
+      let addedDish;
+      const newReview = {
+        rating: req.body.rating,
+        description: req.body.description 
+      }
+      if(req.body.date) {
+        newReview.date = req.body.date;
+      }
+      const newDish = {
+          name: req.body.name
+      }
+      Review.create(newReview)
+      .then(review => Dish.create({
+          restaurant: mongoose.Types.ObjectId(req.params.id),
+          name: req.body.name,
+          reviews: [review._id]
+      }))
+      .then(dish => {
+          addedDish = dish
+          Restaurant.findByIdAndUpdate(req.params.id,{ $push: {"dishes": dish} },{safe: true, upsert: true})
+      })
+      .then(restaurant => res.json(addedDish.serialize()))
+  
+        .catch(err => {
+          console.error(err);
+          res.status(500).json({ error: 'Internal Server Error' });
+        })
+    })
 
 module.exports = {router};
        

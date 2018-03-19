ce41a58e986a62d44e23f4c4d0ea5ec172f394ff
rk-codes <ping.you.back@gmail.com>
2018-03-18 20:44:02 -0700

    add test database url

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
 exports.PORT = process.env.PORT || 8080;
 exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/favoreats-app';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-favoreats-app';
 exports.JWT_SECRET = process.env.JWT_SECRET || 'secret';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';

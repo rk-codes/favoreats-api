5e7d36ce80fdbe28f048dec3de740789b1ce9ecf
rk-codes <ping.you.back@gmail.com>
2018-03-27 12:24:37 -0700

    add env var to heroku

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000'; //add to env variable in heroku
 exports.PORT = process.env.PORT || 8080;
 exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/favoreats-app';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-favoreats-app';
